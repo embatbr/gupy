7649538192580fd07c6ed5be649edf128f2b7a75
Eduardo Tenório <embatbr@gmail.com>
2018-06-05 00:36:42 -0300

    Photographs under a directory.

--- back-end/app/controllers.py
+++ back-end/app/controllers.py
@@ -12,6 +12,21 @@ embody = lambda req: json.loads(str(req.stream.read(req.content_length), 'utf-8'
 
 has_payload = lambda body, f: isinstance(body, f) and body
 
+def extract_body(body_type):
+    try:
+        body = embody(req)
+        if not has_payload(body, body_type):
+            raise Exception('Payload must be a non-empty JSON object')
+
+        return body
+
+    except Exception as err:
+        resp.status = falcon.HTTP_400
+        resp.body = json.dumps({
+            'err': str(err)
+        })
+        return None
+
 
 class Controller(object):
 
@@ -20,23 +35,6 @@ class Controller(object):
 
 
 class ProfileController(Controller):
-
-    def on_get(self, req, resp):
-        params = req.params
-
-        try:
-            profile = self.domains['read'].apply(params)
-
-            resp.status = falcon.HTTP_200
-            resp.body = json.dumps(profile)
-
-        except Exception as err:
-            is_domain_err = isinstance(err, util.DomainError)
-
-            resp.status = falcon.HTTP_403 if is_domain_err else falcon.HTTP_400
-            resp.body = json.dumps({
-                'err': err.show() if is_domain_err else str(err)
-            })
 
     def on_post(self, req, resp):
         body = None
@@ -59,6 +57,23 @@ class ProfileController(Controller):
             resp.body = json.dumps({
                 'message': 'Candidate profile successfully created'
             })
+
+        except Exception as err:
+            is_domain_err = isinstance(err, util.DomainError)
+
+            resp.status = falcon.HTTP_403 if is_domain_err else falcon.HTTP_400
+            resp.body = json.dumps({
+                'err': err.show() if is_domain_err else str(err)
+            })
+
+    def on_get(self, req, resp):
+        params = req.params
+
+        try:
+            profile = self.domains['read'].apply(params)
+
+            resp.status = falcon.HTTP_200
+            resp.body = json.dumps(profile)
 
         except Exception as err:
             is_domain_err = isinstance(err, util.DomainError)
